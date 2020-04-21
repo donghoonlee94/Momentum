2b40353f78b7060e06569ac2e2bc9ebcd71b95b2
donghoonlee94 <94donghoon.lee@gmail.com>
2020-04-21 17:53:12 +0900

    이름 default value로 출력되는 오류 해결

--- gretting.js
+++ gretting.js
@@ -2,7 +2,7 @@ const form = document.querySelector(".js-form"),
   input = form.querySelector("input"),
   greeting = document.querySelector(".js-greetings"),
   todoInputForm = document.querySelector(".js-toDoForm"),
-  todoInput = todoInputForm.querySelector("input");
+  todoListInput = todoInputForm.querySelector("input");
   
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
@@ -14,6 +14,7 @@ function saveName(value) {
 function handleSubmit(event) {
   event.preventDefault();
   const currentValue = input.value;
+  console.log('1',currentValue);
   paintGreeting(currentValue);
   saveName(currentValue);
 }
@@ -27,14 +28,14 @@ function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
   greeting.innerText = `안녕하세요! ${text} 님, 오늘도 좋은 하루 보내세요.`;
-  todoInput.style.display = 'block';    
+  todoListInput.style.display = 'block';   
 }
 
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
     askForName();
-    todoInput.style.display = 'none';
+    todoListInput.style.display = 'none';
   } else {
     paintGreeting(currentUser);
   }
@@ -43,17 +44,14 @@ function loadName() {
 function init() {
   loadName();
   input.addEventListener("focus", function() {
-    input.value = null;
+    input.value = "";
   })
-  input.addEventListener("blur", function () {
-    input.value = '이름이 어떻게 되세요?';
+  todoListInput.addEventListener("focus", function () {
+    todoListInput.value = "";
   })
-  todoInput.addEventListener("focus", function () {
-    todoInput.value = null;
+  todoListInput.addEventListener("blur", function () {
+    todoListInput.value = '오늘은 무엇을 하실건가요?';
   })
-  todoInput.addEventListener("blur", function () {
-    todoInput.value = '오늘은 무엇을 하실건가요?';
-  })     
 }
 
 init();
